03773c47ae04f45d124c5edf79934cb04584f03f
DELL <m.ayyansufian01@gmail.com>
2026-03-17 00:09:57 +0500

    Use abstraction for the practice

--- lib/screen/abstraction_screen.dart
+++ lib/screen/abstraction_screen.dart
@@ -2,19 +2,23 @@ import 'dart:io';
 
 abstract class Engine {
   void power(String power);
+
+  void wheel() {
+    print('4 wheels');
+  }
 }
 
 class Car extends Engine {
   @override
   void power(String power) {
-    print("Car engine power is $power");
+    print('Car engine power is $power');
   }
-
 }
 
 void main() {
   Car car = Car();
-  print("Enter your car hp: ");
-  String? Power = stdin.readLineSync();
-  car.power(Power!);
+  print('Enter your car hp: ');
+  String? power = stdin.readLineSync();
+  car.power(power!);
+  car.wheel();
 }
